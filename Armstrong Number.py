651ec12833c2b9a3dd7fdb6339bff07dffb152c0
Ömer Aydemir <58370530+omeraydmr@users.noreply.github.com>
2020-10-25 15:52:56 +0300

    Update Armstrong Number.py

--- Armstrong Number.py
+++ Armstrong Number.py
@@ -1,6 +1,8 @@
-#Armstrong Number Finder 25.05.2020 written by Masware
+#Armstrong Number Finder 25.05.2020 written by omeraydmr
+#Definition : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself.
+#For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
 
-number = int(input("Please enter your number which want to find armstrong= "))
+number = int(input("Please enter your number which want to find armstrong or not= "))
 digit = list()
 sum = 0
 
