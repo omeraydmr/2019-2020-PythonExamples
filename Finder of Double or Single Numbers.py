c2100afe179f24705fdcb2505bcb833933d398b7
Ömer Aydemir <58370530+omeraydmr@users.noreply.github.com>
2020-10-25 15:55:34 +0300

    Update Finder of Double or Single Numbers.py

--- Finder of Double or Single Numbers.py
+++ Finder of Double or Single Numbers.py
@@ -1,4 +1,4 @@
-#Finder of Double or Single Numbers 1 to 100 25.05.2020 written by Masware
+#Finder of Double or Single Numbers 1 to 100 25.05.2020 written by omeraydmr
 
 double_number_list = [x for x in range(1, 101) if x % 2 == 0]
 single_number_list = [x for x in range(1 , 101) if x % 2 != 0]
