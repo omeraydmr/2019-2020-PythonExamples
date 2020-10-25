326d0a68ee3e28176f737b8372dc2cd521262f8c
Ömer Aydemir <58370530+omeraydmr@users.noreply.github.com>
2020-10-25 16:00:35 +0300

    Update Perfect Number.py

--- Perfect Number.py
+++ Perfect Number.py
@@ -1,4 +1,6 @@
-#Perfect Number Finder 25.05.2020 written by Masware
+#Perfect Number Finder 25.05.2020 written by omeraydmr
+#Definition : In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. 
+#For instance, 6 has divisors 1, 2 and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number.
 
 number = int(input("Please enter your number which want to find perfect= "))
 b = 1
